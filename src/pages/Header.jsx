b63a46667bbce53c66c445f5a19ea7a1faf86707
lordrichard6 <paulolopesreizinho@gmail.com>
2021-05-13 16:02:43 +0200

    typing effect

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,9 +1,25 @@
 import main from "../img/logo_me.png";
+import ReactTypingEffect from 'react-typing-effect';
 
 const Header = () => {
   return (
-    <div className="h-screen flex justify-center flex-wrap content-center">
-      <img className="w-1/4" src={main} alt="" />
+    <div className="h-screen flex flex-col justify-center flex-wrap content-center">
+      <img className="h-3/5 md:h-3/4" src={main} alt="" />
+      <div className="flex content-center justify-center">
+          <ReactTypingEffect 
+                        className='typingeffect' 
+                        text={["My name is Paulo Reizinho",
+                            'Florina is cute', 
+                            'I am a developer', 
+                            'Welcome to my CV page', 
+                            '...', 
+                            'please UX is very simple', 
+                            'click on a section on the left', 
+                            '...' , 
+                            'no need to stay here all day']} 
+                        speed={100} eraseDelay={700} />
+      </div>
+      
     </div>
   );
 };
