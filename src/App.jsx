52dd3adedf1b68c4714d7373da5fc57a65fa3186
lordrichard6 <paulolopesreizinho@gmail.com>
2021-05-16 14:27:57 +0200

    comment out

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from "./pages/Header";
 import About from "./pages/About";
-import Education from "./pages/Education";
-import Experience from "./pages/Experience";
-import Skills from "./pages/Skills";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
+// import Education from "./pages/Education";
+// import Experience from "./pages/Experience";
+// import Skills from "./pages/Skills";
+// import Contact from "./pages/Contact";
+// import Projects from "./pages/Projects";
 
 const App = () => {
   return (
