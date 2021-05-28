3f5679d3bbcb7c86001b0c74671a599736c94590
lordrichard6 <paulolopesreizinho@gmail.com>
2021-05-28 19:05:41 +0200

    remodel about section

---
+++ src/components/AboutPopup.jsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+function AboutPopup({ image, trigger, setTrigger, text }) {
+  return trigger ? (
+    <Popup>
+      <PopupInner>
+        <div className="w-screen md:w-3/5 rounded-lg">
+          <img
+            src={image}
+            alt="me in the jungle"
+            onClick={() => setTrigger(false)}
+          />
+          <span className="w-full m-0 md:mx-4 md:my-2 md:w-5/12">
+            <p>{text}</p>
+          </span>
+        </div>
+      </PopupInner>
+    </Popup>
+  ) : (
+    ""
+  );
+}
+
+export default AboutPopup;
+
+const Popup = styled.div.attrs({})`
+  top: 0;
+  left: 0;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.6);
+  display: flex;
+
+`;
+
+const PopupInner = styled.div.attrs({
+  className: "flex flex-wrap justify-center content-center",
+})`
+  div {
+    position: relative;
+    & {
+      img {
+        border-radius: 0.5rem;
+      }
+      span {
+        position: absolute;
+        background-color: #fff;
+        border-radius: 10px;
+        border: 3px solid;
+        top: 0;
+        left: 0;
+        padding: 3px;
+      }
+    }
+  }
+`;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,38 @@
 import { Parallax } from "react-scroll-parallax";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 import { Section, Title } from "../styles/App";
+import AboutPopup from "../components/AboutPopup";
 
 import title from "img/about_me-min.png";
-import me1 from "img/about_me_02-min.png";
-import me from 'img/about_me_01-min.png'
+// import me1 from "img/about_me_02-min.png";
+import Card_01 from "img/about_me_01-min.png";
+import Card_02 from "img/about_me_02-min.png";
+import Card_03 from "img/about_me_03-min.png";
+import Popup_01 from "img/about_me_wide_01-min.png";
+import Popup_02 from "img/about_me_wide_02-min.png";
+import Popup_03 from "img/about_me_wide_03-min.png";
+import Card_mobile_01 from "img/about_me_mobile_01-min.png";
+import Card_mobile_02 from "img/about_me_mobile_02-min.png";
+import Card_mobile_03 from "img/about_me_mobile_03-min.png";
+import Popup_mobile_01 from "../img/about_me_popup_mobile_01-min.png";
+import Popup_mobile_02 from "../img/about_me_popup_mobile_02-min.png";
+import Popup_mobile_03 from "../img/about_me_popup_mobile_03-min.png";
 
 const About = () => {
-  const [offsetY, setOffsetY] = useState(0);
+  const [popup1, setPopup1] = useState(false);
+  const [popup2, setPopup2] = useState(false);
+  const [popup3, setPopup3] = useState(false);
 
-  const handleScroll = () => setOffsetY(window.pageYOffset);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  const story = {
+    text_01:
+      "My name is Paulo Reizinho, is was born in 1986 in a small town in the middle of Africa. I currently living in Swiss land more precisely in Zurich. I took Administration with focus in accounting but thing is, I love coding and the challenges that come with it but I found it much later, but is never to late to create great things provided that you have what it takes.",
+    text_02:
+      "Ok, i wasn't born in Africa but in Portugal, but the the rest is true 😆. Well, I've been learning web Development since 2019 with highs and lows, took the self-thought route because my financial situation didn't allow to do a bootcamp or more.",
+    text_03:
+      "My main tech is React and my main focus is Front End, but my learning journey will never stop.  Disclaimer, I recently got interested in blockchain and it's ecosytem.",
+  };
 
   return (
     <Section className="bg-about-mobile md:bg-about">
@@ -32,40 +48,93 @@ const About = () => {
       </Title>
 
       <div className="min-h-screen p-2 relative 2xl:px-40 mb-20 max-w-screen">
-        <div className="flex flex-row flex-wrap justify-center content-center">
-          <img src={me} className='w-1/3 filter blur-sm hover:blur-0 animate-pulse hover:animate-none transition duration-500 ease-in' alt="" />
-          <img src={me} className='w-1/3 filter blur-sm hover:blur-0 animate-pulse hover:animate-none transition duration-500 ease-in' alt="" />
-          <img src={me} className='w-1/3 filter blur-sm hover:blur-0 animate-pulse hover:animate-none transition duration-500 ease-in' alt="" />
-        </div>
-        {/* <h3>Ola pessoal,</h3>
-        <div className="flex flex:col flex-wrap md:flex-row justify-center">
-          <p className="md:flex-1 mb-10 md:mb-0 pt-10 leading-relaxed text-white text-xl md:text-2xl 2xl:text-4xl">
-            My name is Paulo Reizinho, I'm portuguese but living in Swiss land
-            more precisely in Zurich. I took Administration with focus in
-            accounting but thing is, I love coding and the challenges that come
-            with it but I found it much later, but is never to late to create
-            great things provided that you have what it takes.
-            <br />
-            Well, I've been learning web Development since 2019 with highs and
-            lows, took the self-thought route because my financial situation
-            didn't allow to do a bootcamp or more.
-            <br />
-            My main tech is React and my main focus is Front End, but my
-            learning journey will never stop.
-            <br />
-            Disclaimer, I recently got interested in blockchain and it's
-            ecosytem.
-          </p>
-          <img
-            src={me1}
-            alt="title"
-            className="hidden md:block h-80 md:w-1/3 md:h-auto md:flex-1 2xl:flex-none rounded-full shadow-2xl"
-            style={{ transform: `translateY(${offsetY * -0.2}px)` }}
+        <div className="hidden md:flex flex-row flex-wrap justify-center content-center">
+          <AboutCard
+            onClick={() => setPopup1(true)}
+            src={Card_01}
+            alt="me in the jungle"
           />
-        </div> */}
+          <AboutCard onClick={() => setPopup2(true)} src={Card_02} alt="sea" />
+          <AboutCard
+            onClick={() => setPopup3(true)}
+            src={Card_03}
+            alt="desert"
+          />
+          <AboutPopup
+            trigger={popup1}
+            setTrigger={setPopup1}
+            image={Popup_01}
+            text={story.text_01}
+          />
+          <AboutPopup
+            trigger={popup2}
+            setTrigger={setPopup2}
+            image={Popup_02}
+            text={story.text_02}
+          />
+          <AboutPopup
+            trigger={popup3}
+            setTrigger={setPopup3}
+            image={Popup_03}
+            text={story.text_03}
+          />
+        </div>
+
+{/* MOBILE ----------------------------------------------------------------------------------------------------- */}
+        <div className="flex md:hidden flex-col flex-wrap justify-center content-center">
+          <AboutCard
+            onClick={() => setPopup1(true)}
+            src={Card_mobile_01}
+            alt="me in the jungle"
+          />
+          <AboutCard onClick={() => setPopup2(true)} src={Card_mobile_02} alt="sea" />
+          <AboutCard
+            onClick={() => setPopup3(true)}
+            src={Card_mobile_03}
+            alt="desert"
+          />
+          <AboutPopup
+            trigger={popup1}
+            setTrigger={setPopup1}
+            image={Popup_mobile_01}
+            text={story.text_01}
+          />
+          <AboutPopup
+            trigger={popup2}
+            setTrigger={setPopup2}
+            image={Popup_mobile_02}
+            text={story.text_02}
+          />
+          <AboutPopup
+            trigger={popup3}
+            setTrigger={setPopup3}
+            image={Popup_mobile_03}
+            text={story.text_03}
+          />
+        </div>
       </div>
     </Section>
   );
 };
 
 export default About;
+
+const AboutCard = styled.img.attrs({
+  className: "w-screen py-4 md:w-1/3 filter hover:animate-none transition duration-500 ease-in",
+})`
+  & {
+    animation: pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+    transition-duration: 200ms;
+
+    @keyframes pulse {
+      0%, 100% {
+        opacity: 1;
+      }
+      50% {
+        opacity: .8;
+      }
+  }
+  :hover {
+    --tw-blur: blur(1px);
+  }
+`;
