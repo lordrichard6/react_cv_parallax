466624cf0d3a95dfc26f5591d8339a76470e2eae
lordrichard6 <paulolopesreizinho@gmail.com>
2021-06-02 15:46:48 +0200

    education section styling code organizing

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,67 +1,51 @@
 import { Parallax } from "react-scroll-parallax";
 import { Slide } from "react-slideshow-image";
 import styled from "styled-components";
+import tw from "twin.macro";
 import "react-slideshow-image/dist/styles.css";
 
 import EducationCard from "components/EducationCard";
-import { Section, Title } from "styles/App";
-import {courses} from 'assets/text'
-
-import {
-  pic01,
-  pic02,
-  pic03,
-  pic04,
-  pic05,
-  pic01_wide,
-  pic02_wide,
-  pic03_wide,
-  pic04_wide,
-  pic05_wide,
-} from "./index";
+import { courses } from "assets/text";
 
 const Education = () => {
   return (
-    <Section className="bg-education">
-      <Title className="md:mt-52 2xl:mt-72">
-        <Parallax
-          y={[-40, 40]}
-          x={[-45, 0]}
-        >
-          <h1 className='text-green-400 text-5xl md:text-7xl'>education();</h1>
+    <Section>
+      <Title>
+        <Parallax y={[-40, 40]} x={[-45, 0]}>
+          <h1>education();</h1>
         </Parallax>
       </Title>
       <EducationContainer>
-        <div className="slide-container w-4/5 md:w-3/4 2xl:w-2/3 rounded-md">
+        <SlideContainer>
           <Slide easing="ease" className="contents md:hidden">
-          {courses.map(n => {
+            {courses.map((n) => {
               return (
                 <EducationCard
-              img={n.img_m}
-              title={n.title}
-              from={n.from}
-              to={n.to}
-              where={n.where}
-              text={n.text}
-            />
-              )
+                  img={n.img_m}
+                  title={n.title}
+                  from={n.from}
+                  to={n.to}
+                  where={n.where}
+                  text={n.text}
+                />
+              );
             })}
           </Slide>
           <Slide easing="ease" className="hidden md:contents">
-            {courses.map(n => {
+            {courses.map((n) => {
               return (
                 <EducationCard
-              img={n.img_w}
-              title={n.title}
-              from={n.from}
-              to={n.to}
-              where={n.where}
-              text={n.text}
-            />
-              )
+                  img={n.img_w}
+                  title={n.title}
+                  from={n.from}
+                  to={n.to}
+                  where={n.where}
+                  text={n.text}
+                />
+              );
             })}
           </Slide>
-        </div>
+        </SlideContainer>
       </EducationContainer>
     </Section>
   );
@@ -69,7 +53,24 @@ const Education = () => {
 
 export default Education;
 
+const Section = styled.section.attrs({
+  className: "py-2 px-2 md:p-6 bg-education bg-center bg-cover",
+})``;
+
+const Title = styled.div.attrs({
+  className:
+    "w-full md:mt-52 2xl:mt-72 flex flex-row justify-center my-28 subpixel-antialiased font-black text-4xl md:text-9xl",
+})`
+  h1 {
+    ${tw`text-green-400 text-5xl md:text-7xl`}
+  }
+`;
+
 const EducationContainer = styled.div.attrs({
   className:
     "flex flex-col flex-wrap justify-center content-center pb-10 2xl:pb-20",
+})``;
+
+const SlideContainer = styled.div.attrs({
+  className: "slide-container w-4/5 md:w-3/4 2xl:w-2/3 rounded-md",
 })``;
