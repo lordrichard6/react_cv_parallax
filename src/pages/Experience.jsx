49a89d012be0b00a91e379df0b92d1c4441eee33
lordrichard6 <paulolopesreizinho@gmail.com>
2021-06-09 14:27:05 +0200

    organize experience section code

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,5 +1,6 @@
 import { Parallax } from "react-scroll-parallax";
 import styled from "styled-components";
+import tw from "twin.macro";
 
 import JobExperience from "components/JobExperience";
 import { job } from "assets/main";
@@ -7,17 +8,17 @@ import { job } from "assets/main";
 const Experience = () => {
   return (
     <Section className="bg-experience">
-      <Title>
+      <TitleContainer>
         <Parallax y={[-40, 10]} x={[-100, 5]}>
-          <h1 className="text-red-600">{"<h1>"}</h1>
+          <RedTitle>{"<h1>"}</RedTitle>
         </Parallax>
         <Parallax y={[45, -40]} x={[0, -43]}>
-          <h1 className="text-purple-600 lg:text-6xl">experience</h1>
+          <MiddleTitle>experience</MiddleTitle>
         </Parallax>
         <Parallax y={[-40, 0]} x={[-100, 0]}>
-          <h1 className="text-red-600">{"</h1>"}</h1>
+          <RedTitle>{"</h1>"}</RedTitle>
         </Parallax>
-      </Title>
+      </TitleContainer>
       <JobContainer>
         <JobExperience
           title={job[0].title}
@@ -34,14 +35,50 @@ const Experience = () => {
 
 export default Experience;
 
-const JobContainer = styled.div.attrs({
-  className: "flex flex-wrap justify-center content-center",
-})``;
+const JobContainer = styled.div`
+  ${tw`
+    flex 
+    flex-wrap 
+    justify-center 
+    content-center
+  `}
+`;
 
-const Section = styled.section.attrs({
-  className: "py-2 px-2 md:p-6 bg-center bg-cover"
-})``;
+const Section = styled.section`
+  ${tw`
+    py-2 
+    px-2 
+    md:p-6 
+    bg-center 
+    bg-cover
+  `}
+`;
 
-const Title = styled.div.attrs({
-  className: "w-full flex flex-row justify-center my-28 md:mb-56 subpixel-antialiased font-black text-4xl lg:text-9xl"
-})``;
+const TitleContainer = styled.div`
+  ${tw`
+    w-full 
+    flex 
+    flex-row 
+    justify-center 
+    my-28 
+    md:mb-56 
+    subpixel-antialiased 
+    font-black 
+    text-4xl 
+    lg:text-9xl
+  `}
+`;
+
+const RedTitle = styled.h1`
+  ${tw`
+    text-red-600
+  `}
+`
+
+const MiddleTitle = styled.h1`
+  ${tw`
+    text-purple-600 
+    lg:text-6xl
+  `}
+`
+
